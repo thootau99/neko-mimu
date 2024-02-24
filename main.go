f3b041168f789217ca3d09f203f4d4b4dd604a30
thootau <burkekhoo@gmail.com>
2024-02-24 16:57:32 +0000

    feat: 文字をビデオにオーバーレイのメソッドを分離

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	methods "github.com/thootau/neko-mimu/methods"
 )
@@ -10,11 +11,13 @@ func main() {
 
 	const _TEMPLATE_PATH = "./templates"
 	const _OUTPUT_PATH = "./output"
-
-	methods.OverlayVideoOnVideo(
+	methods.ImageToVideoWithSecond(fmt.Sprintf("%s/single_room.jpg", _TEMPLATE_PATH), 10)
+	outputPath := methods.OverlayVideoOnVideo(
 		fmt.Sprintf("%s/banana.mp4", _TEMPLATE_PATH),
-		fmt.Sprintf("%s/single_room.jpg", _TEMPLATE_PATH),
+		fmt.Sprintf("%s/single_room.mp4", _TEMPLATE_PATH),
 		1,
 	)
+	time.Sleep(1000)
+	methods.PutTextOnVide(outputPath, "割", 10, 10)
 
 }
--- methods/ffmpeg_method.go
+++ methods/ffmpeg_method.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 
 	ffmpeg "github.com/u2takey/ffmpeg-go"
+	"github.com/u2takey/go-utils/uuid"
 )
 
 func ImageToVideoWithSecond(imagePath string, second int) {
@@ -16,14 +17,26 @@ func ImageToVideoWithSecond(imagePath string, second int) {
 	}
 }
 
-func OverlayVideoOnVideo(frontVideoPath string, backgroundVideoPath string, startSecond int) {
+func OverlayVideoOnVideo(frontVideoPath string, backgroundVideoPath string, startSecond int) string {
+	outputFileNameUUID := uuid.NewUUID()
+	outputPath := fmt.Sprintf("./output/%s.mp4", outputFileNameUUID)
 	overlay := ffmpeg.Input(frontVideoPath).Filter("colorkey", ffmpeg.Args{"0x4fff00:0.1:0.2"})
 	err := ffmpeg.Filter(
 		[]*ffmpeg.Stream{
 			ffmpeg.Input(backgroundVideoPath),
 			overlay,
-		}, "overlay", ffmpeg.Args{"1:1"}, ffmpeg.KwArgs{"enable": "gte(t,1)"}).
-		Output("./output/out1.mp4").OverWriteOutput().ErrorToStdOut().Run()
+		}, "overlay", ffmpeg.Args{"1:1"}, ffmpeg.KwArgs{"enable": "gte(t,1)"}).Output(outputPath).OverWriteOutput().ErrorToStdOut().Run()
+
+	if nil != err {
+		log.Fatal(err)
+	}
+
+	return outputPath
+}
+
+func PutTextOnVide(videoPath string, text string, x int, y int) {
+	err := ffmpeg.Input(videoPath).Drawtext("割", 10, 10, true, ffmpeg.KwArgs{"fontfile": "/font/NotoSansCJKjp-Regular.otf", "fontsize": "240"}).Output(videoPath).OverWriteOutput().ErrorToStdOut().Run()
+
 	if nil != err {
 		log.Fatal(err)
 	}
