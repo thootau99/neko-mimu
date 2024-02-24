4d6d603a3831a638633539598cc5d8923577fc54
ShiriNmi1520 <s20026352@gmail.com>
2024-02-25 02:46:24 +0800

    :sparkles: VideoContent 新增Function用於檢查傳入資料正確性 匯出部分VideoContent欄位以便後續ffmpeg使用

--- pkg/videoContent/videoContent.go
+++ pkg/videoContent/videoContent.go
@@ -1,15 +1,39 @@
 package videoContent
 
 type VideoContent struct {
-	contentUri   string
-	second       int
+	ContentUri   string
+	Second       int
 	isBackground bool
-	layer        int
-	scale        float32
-	position     Position
+	Layer        int
+	Scale        float32
+	Position     Position
 }
 
 type Position struct {
 	x float32
 	y float32
+}
+
+func GetBackgroundContentCount(videoContents []VideoContent) int {
+	count := 0
+	for _, videoContent := range videoContents {
+		if videoContent.isBackground {
+			count++
+		}
+	}
+	return count
+}
+
+func GetLayerOneCount(videoContents []VideoContent) int {
+	count := 0
+	for _, videoContent := range videoContents {
+		if videoContent.Layer == 1 {
+			count++
+		}
+	}
+	return count
+}
+
+func ValidateContent(videoContents []VideoContent) bool {
+	return GetBackgroundContentCount(videoContents) == 1 && GetLayerOneCount(videoContents) == 1
 }
