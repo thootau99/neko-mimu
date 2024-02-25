f299b08394a341ba883a32f3884f134645e9e98c
ShiriNmi <s20026352@gmail.com>
2024-02-25 22:48:14 +0800

    :sparkles: Content validator 新增內容檢查(覆蓋，有無背景，層級)

--- pkg/videoContent/videoContent.go
+++ pkg/videoContent/videoContent.go
@@ -1,6 +1,9 @@
 package videoContent
 
-import "time"
+import (
+	"log/slog"
+	"time"
+)
 
 type VideoContent struct {
 	ContentUri   string
@@ -42,5 +45,42 @@ func GetLayerOneCount(videoContents []VideoContent) int {
 
 // ValidateContent TODO: Could be improved by adding more validation rules
 func ValidateContent(videoContents []VideoContent) bool {
+	backgrounds := GetVideoBackground(videoContents)
+	if len(backgrounds) < 1 {
+		slog.Warn("Missing background content, will use default background")
+	}
+	if GetLayerOneCount(videoContents) > 1 {
+		slog.Warn("Multiple layer one content detected, some content might not be visible")
+	}
+	if IsBackgroundOverlapping(videoContents) {
+		slog.Warn("Backgrounds overlapping detected, some content might not be visible")
+	}
 	return GetBackgroundContentCount(videoContents) == 1 && GetLayerOneCount(videoContents) == 1
+}
+
+func (videoContent *VideoContent) SetBackground() {
+	videoContent.isBackground = true
+}
+
+func GetVideoBackground(videoContents []VideoContent) []VideoContent {
+	var backgrounds []VideoContent
+	for _, videoContent := range videoContents {
+		if videoContent.isBackground {
+			backgrounds = append(backgrounds, videoContent)
+		}
+	}
+	return backgrounds
+}
+
+func IsBackgroundOverlapping(videoContents []VideoContent) bool {
+	backgrounds := GetVideoBackground(videoContents)
+	// Calculate StartTime and duration to check if they are overlapping on editor (StartTime and EndTime)
+	for i := 0; i < len(backgrounds); i++ {
+		for j := i + 1; j < len(backgrounds); j++ {
+			if backgrounds[i].StartTime.Before(backgrounds[j].EndTime) && backgrounds[i].EndTime.After(backgrounds[j].StartTime) {
+				return true
+			}
+		}
+	}
+	return false
 }
